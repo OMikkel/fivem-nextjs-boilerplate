776450d80754f90e198e72dfd8726a99e1694c8a
OMikkel <mikkelmaae@gmail.com>
2023-11-25 12:09:54 +0100

    Redo project structure

---
+++ fivem/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version "cerulean"
+
+game "gta5"
+
+description "FiveM NUI NextJS Boilerplate - Made by OMikkel#3217"
+
+author "OMikkel#3217"
+
+client_scripts {
+    "client.lua"
+}
+
+server_scripts {
+    "server.lua"
+}
+
+ui_page "<VERCEL LINK HERE>"
